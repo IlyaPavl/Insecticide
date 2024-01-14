4775cbd067eb68facfddf8afad55bf2115d1dfbd
Ilya Pavlov <ilyapav@icloud.com>
2024-01-14 15:53:04 +0300

    Initial Commit

---
+++ Insecticide/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Insecticide
+//
+//  Created by ily.pavlov on 14.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
